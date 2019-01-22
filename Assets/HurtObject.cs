acb686bcccf103edba4f73ca7c1ac2d499493665
Marcus Salér <marcus.saler@elev.ga.lbs.se>
2019-01-22 10:41:12 +0100

    Fiender
    complete

---
+++ Assets/HurtObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HurtObject : MonoBehaviour
+{
+    
+    public string load = "Level 2";
+
+
+    private void OnTriggerEnter2D(Collider2D collision) 
+    {
+        if(collision.tag == "Player") 
+        {
+            
+            print("You died"); 
+            
+            SceneManager.LoadScene(load); 
+        }
+    }
+}
---
+++ Assets/SetInvisible.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetInvisible : MonoBehaviour
+{
+    void Start()
+    {
+
+        GetComponent<SpriteRenderer>().enabled = false;
+    }
+}
