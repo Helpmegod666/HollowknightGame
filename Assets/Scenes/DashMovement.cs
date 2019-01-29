9bd95864daf91dfb307d7f81ecbffbef283a617b
Kevin Donoso <kevin.donoso@elev.ga.lbs.se>
2019-01-29 10:03:35 +0100

    movment / (icke fungerade dash)

---
+++ Assets/Scenes/DashMovement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float dashSpeed;
+    private float dashTime;
+    public float startDashTime;
+    private int direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        dashTime = startDashTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (direction == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                direction = 1;
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                direction = 2;
+            }
+            else if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                direction = 3;
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                direction = 4;
+            }
+            else
+            {
+                if (dashTime <= 0)
+                {
+                    direction = 0;
+                    dashTime = startDashTime;
+                    rb.velocity = Vector2.zero;
+                }
+                else
+                {
+                    dashTime -= Time.deltaTime;
+                    if(direction == 1)
+                    {
+                        rb.velocity = Vector2.left * dashSpeed;
+                    }                   
+                   else if (direction == 2)
+                    {
+                        rb.velocity = Vector2.right * dashSpeed;
+                    }
+                   else if (direction == 3)
+                    {
+                        rb.velocity = Vector2.up * dashSpeed;
+                    }                  
+                   else if (direction == 4)
+                    {
+                        rb.velocity = Vector2.down * dashSpeed;
+                    }
+
+                }
+                 
+            }
+        }
+
+    }
+}
---
+++ Assets/Scenes/GroundChecker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundChecker : MonoBehaviour
+{
+    public int isgrounded;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Ground")
+        {
+            isgrounded = isgrounded + 1;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Ground")
+        {
+            isgrounded = isgrounded - 1;
+        }
+    }
+
+    private void Update()
+    {
+        print(isgrounded);
+    }
+}
