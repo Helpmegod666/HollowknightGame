e4a4123822ca6d4379fbfb0c26fba95fe2ce8b15
LEARNET\david.lindstrom <david.lindstrom@elev.ga.lbs.se>
2019-02-26 09:03:27 +0100

    Gjorde particle system för när man landar.
    Möjligtvis att jag har ändrat på map och annat onödigt (Bara ta bort de ändringarna)

---
+++ Assets/Scripts/HeartDisabler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartDisabler : MonoBehaviour
+{
+    public GameObject heart1;
+    public GameObject heart2;
+    public GameObject heart3;
+
+    void Update()
+    {
+        if (Hp_scipt.Hpremaining == 3)
+        {
+            heart1.gameObject.SetActive(true);
+            heart2.gameObject.SetActive(true);
+            heart3.gameObject.SetActive(true);
+        }
+
+        if (Hp_scipt.Hpremaining == 2)
+        {
+            heart1.gameObject.SetActive(true);
+            heart2.gameObject.SetActive(true);
+            heart3.gameObject.SetActive(false);
+        }
+
+        if (Hp_scipt.Hpremaining == 1)
+        {
+            heart1.gameObject.SetActive(true);
+            heart2.gameObject.SetActive(false);
+            heart3.gameObject.SetActive(false);
+        }
+
+        if (Hp_scipt.Hpremaining == 0)
+        {
+            heart1.gameObject.SetActive(false);
+            heart2.gameObject.SetActive(false);
+            heart3.gameObject.SetActive(false);
+        }
+    }
+}
