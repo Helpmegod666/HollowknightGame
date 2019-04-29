f717160037eb83ca5d53e3b0d0bd5ae3fe0e71fb
LEARNET\david.lindstrom <david.lindstrom@elev.ga.lbs.se>
2019-04-29 10:04:26 +0200

    Spitenemy_p1_David
    yet

---
+++ Assets/Spitmovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spitmovement : MonoBehaviour
+{
+    private Vector2 Direction;
+
+    PlayerMovement player;
+    Rigidbody2D rb;
+    Hp_scipt hp;
+
+    public float spitspeed = 15;
+
+    private void Start()
+    {
+        player = GameObject.FindObjectOfType<PlayerMovement>();
+        rb = GetComponent<Rigidbody2D>();
+        hp = player.GetComponent<Hp_scipt>();
+        Direction = (player.transform.position - transform.position).normalized * spitspeed;
+        rb.velocity = new Vector2(Direction.x, Direction.y);
+        Destroy(gameObject, 3f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            hp.Hpremaining = hp.Hpremaining - 1;
+            Destroy(gameObject);
+        }
+    }
+}
