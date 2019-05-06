1b578abac8de67285adaeaef9e11ee0e4e9454ed
LEARNET\david.lindstrom <david.lindstrom@elev.ga.lbs.se>
2019-05-06 10:31:54 +0200

    Gjorde en härlig PS på spit

--- Assets/Spitmovement.cs
+++ Assets/Spitmovement.cs
@@ -6,11 +6,15 @@ public class Spitmovement : MonoBehaviour
 {
     private Vector2 Direction;
 
+    private float timer = 0;
+
+    public GameObject spit;
     PlayerMovement player;
     Rigidbody2D rb;
     Hp_scipt hp;
 
     public float spitspeed = 15;
+    public float ricochetspitspeed = 10;
 
     private void Start()
     {
@@ -19,7 +23,6 @@ public class Spitmovement : MonoBehaviour
         hp = player.GetComponent<Hp_scipt>();
         Direction = (player.transform.position - transform.position).normalized * spitspeed;
         rb.velocity = new Vector2(Direction.x, Direction.y);
-        Destroy(gameObject, 3f);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -27,7 +30,25 @@ public class Spitmovement : MonoBehaviour
         if (collision.tag == "Player")
         {
             hp.Hpremaining = hp.Hpremaining - 1;
+            Instantiate(spit, transform.position, spit.transform.rotation);
+            Destroy(gameObject, 0.05f);
+        }
+
+        if (collision.tag == "Ground")
+        {
+            Instantiate(spit, transform.position, spit.transform.rotation);
+            Destroy(gameObject, 0.05f);
+        }
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer >= 3)
+        {
             Destroy(gameObject);
         }
     }
+
 }
